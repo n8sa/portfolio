d78da57f8a1a56b48e89ca5d605e7503e1c44379
farah_nisa syahhindah <farahnisasyahindah@gmail.com>
2025-11-20 17:10:47 +0000

    no need about me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
 import { Hero } from '@/components/sections/hero';
-import { About } from '@/components/sections/about';
 import { Projects } from '@/components/sections/projects';
 import { Skills } from '@/components/sections/skills';
 import { Contact } from '@/components/sections/contact';
@@ -14,7 +13,6 @@ export default function Home() {
       <Header />
       <main className="flex-1">
         <Hero />
-        <About />
         <Experience />
         <Projects />
         <Skills />
